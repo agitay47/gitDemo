4b300d1aa722246c34d22f9fd96c96a455f2dbf2
agitay47 <68969092+agitay47@users.noreply.github.com>
2020-08-01 13:04:03 -0700

    Update BookARoom.java

--- src/BookARoom.java
+++ src/BookARoom.java
@@ -2,6 +2,8 @@ public class BookARoom {
     public static void main(String[] args) {
 
         System.out.println("We are having fun today");
+        
+        //I can add codes on GitHub now
 
     }
 }
