ae183403517f8b5a560ff76bcb50d1ee64f4a3e9
agitay <47agitay@gmail.com>
2020-08-01 08:54:13 -0700

    update HelloB20

--- src/HelloB20.java
+++ src/HelloB20.java
@@ -2,6 +2,11 @@ public class HelloB20 {
     public static void main(String[] args) {
 
         System.out.println("B20 is always happy");
+        int sum=0;
+        for(int i=0; i<=5; i++){
+            sum +=i;
+        }
+        System.out.println(sum);
 
 
 
